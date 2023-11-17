708c80639346aa688eed9a45ed7f488940f4f180
jemprojects <jemprojectsgit@gmail.com>
2023-11-17 18:06:12 -0300

    form and post desde el front

--- tasksfront/src/components/TaskForm.tsx
+++ tasksfront/src/components/TaskForm.tsx
@@ -1,6 +1,64 @@
-function TaskForm(){
-    return (
-        <div>Task Form</div>
-    )
+import { ChangeEvent, useState } from 'react';
+import { createTaskRequest } from '../api/task';
+// import { useTasks } from '../context/useTasks';
+
+function TaskForm() {
+  const [task, setTask] = useState({
+    title: '',
+    description: '',
+    done: false,
+  });
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setTask({ ...task, [e.target.name]: e.target.value });
+  };
+  // const { createTask } = useTasks();
+
+  const handleSubmit = async (e: ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // await createTask(task);
+    console.log(task);
+    const res = await createTaskRequest(task);
+    const data = await res.json();
+    console.log(data);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        name="title"
+        type="text"
+        placeholder="Write a task"
+        onChange={handleChange}
+        className="border-2 border-gray-700 p-2 rounded-lg bg-zinc-800 block w-full my-2"
+      />
+
+      <textarea
+        name="description"
+        rows={3}
+        onChange={handleChange}
+        className="border-2 border-gray-700 p-2 rounded-lg bg-zinc-800 block w-full my-2"
+        placeholder="Write a description"
+      ></textarea>
+
+      <label className="inline-flex items-center gap-x-2">
+        <input
+          type="checkbox"
+          value={task.done ? 1 : 0}
+          onChange={() =>
+            setTask({
+              ...task,
+              done: !task.done,
+            })
+          }
+          className="form-checkbox h-5 w-5 text-indigo-600 transition duration-150 ease-in-out "
+        />
+        <span>Done</span>
+      </label>
+
+      <button type="submit" className="bg-indigo-500 px-3 block py-2 w-full">
+        Save
+      </button>
+    </form>
+  );
 }
-export default TaskForm
+
+export default TaskForm;
